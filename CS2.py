bb5aafa53b7a02a31f383f612c31cf0aa972bcb5
Thomas Utterback <timba@noisycrow.com>
2016-10-03 14:03:39 -0700

    First commit

---
+++ CS2.py
@@ -0,0 +1,25 @@
+# Describe the sketch comedy group
+name = 'Monty Python'
+description = 'sketch comedy group'
+year = 1969
+
+# Describe Monty Python's work
+famous_sketch1 = "\n\tHell's Grannies"
+famous_sketch2 = "\n\tDead Parrot"
+
+# Introduce them
+
+sentence = name + ' is a ' + description + ' formed in ' + str(year)
+print(sentence)
+print('Famous Work:', famous_sketch1, famous_sketch2)
+
+# Calculate the halfway Index 
+word1 = 'Good'
+half1 = len(word1)//2
+end1 = word1[half1:]
+
+word2 = 'Evening'
+half2 = len(word2)//2
+end2 = word2[half2:]
+
+print(end1,end2)
