c6a7d6f9742f30e8e252f8fb63c9b0bda043558f
Tom Utterback <tutterba@starbucks.com>
2016-10-04 13:55:50 -0700

    Add file 3

---
+++ CS3.py
@@ -0,0 +1,11 @@
+slang = ['cheerio', 'pip pip', 'smashing']
+print(slang)
+slang.append('cheeky')
+slang.append('yonks')
+print(slang)
+slang.remove('cheeky')
+print(slang)
+del slang[3]
+print(slang)
+del slang[:2]
+print(slang)
