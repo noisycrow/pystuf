c522c9a3848f68ca422a6cd367e1eb980c5b93ce
Tom Utterback <tutterba@starbucks.com>
2016-10-04 14:20:29 -0700

    added dictionaries to CS3

--- CS3.py
+++ CS3.py
@@ -9,3 +9,12 @@ del slang[3]
 print(slang)
 del slang[:2]
 print(slang)
+
+print('Dictionaries')
+slang = {'cheerio':'goodbye','knackered':'tired'}
+result = slang.get('bloody')
+
+if result:
+    print(result)
+else:
+    print("Key doesn't exist")
