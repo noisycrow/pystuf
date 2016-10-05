68206e51e578f86f8e745a8a6689e61434167e73
Thomas Utterback <timba@noisycrow.com>
2016-10-04 21:52:12 -0700

    finishing CS3-3

--- CS3-3.py
+++ CS3-3.py
@@ -13,4 +13,28 @@ for item in menu:
     menu_prices[item] = price
     price = price + 1
 
-print(menu_prices)
+for name, price in menu_prices.items():
+    print(name, ': $', format(price, '.2f'), sep='')
+
+x = 1
+while x != 3:
+    print('x is', x)
+    x = x + 1
+
+orders = []
+
+
+while (True):
+    order = input("What would you like to order? (Q for Quit)")
+    if order == 'Cheeky Spam':
+        print("Sorry, we're out of that!")
+        continue
+    if order.upper() == 'Q':
+        break
+    found = menu_prices.get(order)
+    if found:
+        orders.append(order)
+    else:
+        print("Menu item doesn't exist")
+
+print("You ordered:", orders)
