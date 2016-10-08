c77ff352d413d013b67f04fc4739f04a1d5f94c2
Thomas Utterback <timba@noisycrow.com>
2016-10-07 20:27:22 -0700

    DL'd code from CodeSchool

--- CodeSchool/Treasuregram/Treasuregram/wsgi.py
+++ CodeSchool/Treasuregram/Treasuregram/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for Treasuregram project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
--- CodeSchool/Treasuregram/main_app/views.py
+++ CodeSchool/Treasuregram/main_app/views.py
@@ -1,26 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Treasure
 
-# Create your views here.
 def index(request):
-#    name = 'Gold Nugget'
-#    value = 1000.00
-#    context = {'treasure_name': name, 'treasure_val': value}
-#    return render(request, 'index.html', context)
     treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
-"""
-class Treasure:
-    def __init__(self, name, value, material, location, img_url):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-        self.img_url = img_url
-
-treasures = [
-    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM",""),
-    Treasure("Fool's Gold", 0, "pyrite", "Fool's Falls, CO",""),
-    Treasure("Coffee Can", 20.00, 'tin', "Acme, CA","")
-]
-"""
